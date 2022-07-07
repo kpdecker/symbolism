b2ad1c2e17f769814bb678fe9afa5d8aafbe7090
kpdecker <kpdecker@gmail.com>
2022-07-07 18:13:52 -0500

    Implement misc utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,170 @@
-import * as ts from 'typescript';
+import invariant from 'tiny-invariant';
+import ts from 'typescript';
+import { dumpNode, dumpSymbol } from './symbols';
+
+export function isArraySymbol(symbol: ts.Symbol): boolean {
+  // const arraySymbol = checker
+  //   .getSymbolsInScope(node, ts.SymbolFlags.Type)
+  //   .find((s) => s.name === 'Array');
+
+  // return type.symbol === arraySymbol;
+
+  return symbol.getName() === 'Array';
+}
+
+export function isErrorType(type: ts.Type | undefined): boolean {
+  return (type as any)?.intrinsicName === 'error';
+}
+
+const intrinsicTypes =
+  ts.TypeFlags.Any |
+  ts.TypeFlags.Unknown |
+  ts.TypeFlags.String |
+  ts.TypeFlags.Number |
+  ts.TypeFlags.BigInt |
+  ts.TypeFlags.Boolean |
+  ts.TypeFlags.BooleanLiteral |
+  ts.TypeFlags.ESSymbol |
+  ts.TypeFlags.Void |
+  ts.TypeFlags.Undefined |
+  ts.TypeFlags.Null |
+  ts.TypeFlags.Never |
+  ts.TypeFlags.NonPrimitive;
+
+export function isIntrinsicType(type: ts.Type) {
+  return (type.flags & intrinsicTypes) !== 0;
+}
+
+export function getPropertySymbol(
+  node: ts.Node,
+  type: ts.Type,
+  checker: ts.TypeChecker,
+  name: string,
+  {
+    stringIndex,
+    numberIndex,
+  }: { stringIndex?: boolean; numberIndex?: boolean } = {}
+) {
+  const symbol =
+    type.getProperty(name) ||
+    (stringIndex && type.getStringIndexType()?.symbol) ||
+    (numberIndex && type.getNumberIndexType()?.symbol) ||
+    undefined;
+
+  let propertyType: ts.Type | undefined = undefined;
+  if (symbol) {
+    const declaration = symbol.declarations?.[0];
+    if (ts.isBindingElement(node) && declaration) {
+      propertyType = checker.getTypeAtLocation(declaration);
+    } else {
+      propertyType = checker.getTypeOfSymbolAtLocation(symbol, node);
+    }
+  }
+
+  // Two distinct objects here lets us track both the property in code and
+  // the ultimate type that it resolves to.
+  return { symbol, type: propertyType };
+}
+
+export function namedPathToNode(
+  node: ts.Node,
+  checker: ts.TypeChecker,
+  filter: (node: ts.Node) => boolean = (node) => !!node,
+  child?: ts.Node
+): string {
+  //
+  if (ts.isSourceFile(node)) {
+    return '';
+  }
+
+  // Stop iterating when we reach a reference
+  if (ts.isCallExpression(node) || ts.isBinaryExpression(node)) {
+    return '';
+  }
+
+  if (
+    ts.isBlock(node) ||
+    ts.isModuleBlock(node) ||
+    ts.isReturnStatement(node) ||
+    ts.isParenthesizedExpression(node) ||
+    ts.isVariableDeclarationList(node) ||
+    ts.isExpressionStatement(node) ||
+    ts.isBinaryExpression(node) ||
+    ts.isNewExpression(node) ||
+    ts.isJsxSelfClosingElement(node) ||
+    ts.isObjectBindingPattern(node) ||
+    ts.isTryStatement(node) ||
+    ts.isCatchClause(node) ||
+    ts.isAwaitExpression(node) ||
+    ts.isObjectLiteralExpression(node) ||
+    //
+    // Types
+    ts.isFunctionTypeNode(node) ||
+    ts.isParenthesizedTypeNode(node) ||
+    ts.isTypeLiteralNode(node) ||
+    //
+    node.kind === ts.SyntaxKind.FirstStatement
+  ) {
+    return namedPathToNode(node.parent, checker, filter, node);
+  }
+
+  let parentPath = '';
+  if (filter(node.parent)) {
+    parentPath = namedPathToNode(node.parent, checker, filter, node) + '.';
+  }
+
+  // Avoid duplicate identifiers
+  if (ts.isIdentifier(node)) {
+    if ((node.parent as any).name?.getText() === node.getText()) {
+      return namedPathToNode(node.parent, checker, filter, node);
+    }
+  }
+
+  let name = '';
+  const symbol = checker.getSymbolAtLocation(node);
+  if (symbol) {
+    name = symbol.name;
+  } else if (
+    ts.isFunctionDeclaration(node) ||
+    ts.isMethodDeclaration(node) ||
+    ts.isInterfaceDeclaration(node) ||
+    ts.isModuleDeclaration(node) ||
+    ts.isEnumDeclaration(node) ||
+    ts.isEnumMember(node)
+  ) {
+    name = node.name?.getText() || name;
+  } else if (
+    ts.isVariableDeclaration(node) ||
+    ts.isPropertySignature(node) ||
+    ts.isJsxAttribute(node) ||
+    ts.isTypeAliasDeclaration(node)
+  ) {
+    if (ts.isIdentifier(node.name)) {
+      name = node.name.text;
+    } else {
+      // TODO: Finish
+      name = node.name.getText();
+    }
+  } else if (ts.isPropertyAssignment(node)) {
+    name = node.name.getText();
+  } else if (ts.isParameter(node)) {
+    name = '[' + node.parent.parameters.indexOf(node) + ']';
+  } else if (ts.isArrayLiteralExpression(node) || ts.isArrayTypeNode(node)) {
+    name = '[]';
+  } else if (ts.isIfStatement(node)) {
+    name = 'if';
+  } else if (ts.isJsxElement(node)) {
+    name = '<' + node.openingElement.tagName.getText() + '>';
+  } else if (ts.isTypeParameterDeclaration(node)) {
+    name = '<' + node.name.getText() + '>';
+  } else {
+    // TODO: Better naming here
+    name = ts.SyntaxKind[node.kind] + '(' + name + ')';
+  }
+  // name = ts.SyntaxKind[node.kind] + '(' + name + ')';
+
+  return parentPath + name;
+}
 
 // Via https://github.com/microsoft/TypeScript/blob/5d65c4dc26334ec7518d2472a9b3b69dac9ff2b5/src/compiler/program.ts#L2667
 /** Returns a token if position is in [start-of-leading-trivia, end), includes JSDoc only in JS files */
