7a0547daa1904221fcc4a67d4391d49c0a163795
kpdecker <kpdecker@gmail.com>
2022-07-17 10:52:46 -0500

    Make log level runtime configurable

--- src/logger.ts
+++ src/logger.ts
@@ -1,15 +1,39 @@
+export enum LogLevel {
+  debug,
+  verbose,
+  info,
+  warn,
+  error,
+}
+
+let loggerLevel = LogLevel.warn;
+
+export function setLogLevel(level: LogLevel) {
+  const originalLevel = loggerLevel;
+  loggerLevel = level;
+  return originalLevel;
+}
+
 export function logWarn(...message: unknown[]) {
-  console.warn(...message);
+  if (loggerLevel <= LogLevel.warn) {
+    console.warn(...message);
+  }
 }
 
 export function logInfo(...message: unknown[]) {
-  // console.log(...message);
+  if (loggerLevel <= LogLevel.info) {
+    console.info(...message);
+  }
 }
 
 export function logVerbose(...message: unknown[]) {
-  // console.log(...message);
+  if (loggerLevel <= LogLevel.verbose) {
+    console.info(...message);
+  }
 }
 
 export function logDebug(...message: unknown[]) {
-  // console.debug(...message);
+  if (loggerLevel <= LogLevel.debug) {
+    console.debug(...message);
+  }
 }
