5ec776aab07c264e789b94e2b3edd1050997261d
kpdecker <kpdecker@gmail.com>
2022-07-19 13:14:32 -0500

    Add symbol table test

---
+++ src/tests/symbol.test.ts
@@ -0,0 +1,32 @@
+import { parseConfig } from "../config";
+import { namedPathToNode } from "../path/index";
+import { dumpSymbolTable, parseSymbolTable } from "../symbols";
+import { initTypescript } from "../typescript";
+
+describe("symbol table", () => {
+  it("should parse this project", () => {
+    const config = parseConfig("./.token-cov.json");
+    const services = initTypescript(config);
+    const program = services.getProgram()!;
+    const checker = program.getTypeChecker();
+
+    const symbolTable = parseSymbolTable(program, config);
+
+    expect(symbolTable.size).toBeGreaterThan(100);
+
+    const artifact = Array.from(symbolTable).find(([symbol, references]) => {
+      return symbol.getName() === "parseSymbolTable";
+    });
+
+    expect(
+      namedPathToNode(artifact![0].declarations![0], checker)
+    ).toMatchInlineSnapshot(`"parseSymbolTable"`);
+
+    const dump = Array.from(dumpSymbolTable(symbolTable, checker));
+    const dumpArtifact = dump.find(
+      ([declaration]) => declaration.name === "parseSymbolTable"
+    )!;
+
+    expect(dumpArtifact[1].length).toBeGreaterThan(1);
+  });
+});
