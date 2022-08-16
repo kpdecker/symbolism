055766ec8609d883d8075c6492163ec959c6dfdd
kpdecker <kpdecker@gmail.com>
2022-08-16 13:29:17 -0500

    Add log level cli parameter

--- packages/symbolism-cli/src/index.ts
+++ packages/symbolism-cli/src/index.ts
@@ -1,18 +1,27 @@
-import { program } from "commander";
+import { Option, program } from "commander";
 import { initCoverage } from "./actions/coverage";
 import { initDumpFiles } from "./actions/dump-files";
 import { initDumpSymbols } from "./actions/dump-symbols";
 
-import { loadCliConfig } from "@symbolism/utils";
+import { loadCliConfig, LogLevel, setLogLevel } from "@symbolism/utils";
 import { initCallInputs } from "./actions/call-inputs";
 import { initDumpSchema } from "./actions/dump-schema";
 import { initDefineSymbol } from "./actions/define-symbol";
 
 program.option("-c, --config <path>", "config file path", "./.symbolism.json");
 
+program.addOption(
+  new Option("--log-level <logLevel>", "Logging level")
+    .choices(Object.keys(LogLevel).filter((key) => !/^\d+$/.test(key)))
+    .default("info")
+);
+program.option("--verbose", "enable verbose logging");
+
 program.hook("preAction", (command) => {
   const opts = command.opts();
   loadCliConfig(opts.config);
+
+  setLogLevel(LogLevel[opts.logLevel] as unknown as LogLevel);
 });
 
 initCoverage(program);
